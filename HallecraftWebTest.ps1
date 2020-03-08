6c2d3f427367e4c681577ad22d5140ff34a87369
Doh Halle <52863523+nwana-halle@users.noreply.github.com>
2020-03-08 02:18:45 +0100

    Add files via upload

---
+++ HallecraftWebTest.ps1
@@ -0,0 +1,38 @@
+﻿Configuration HallecraftWebTest 
+{
+
+    # Import the module that contains the resources we're using.
+    Import-DscResource -ModuleName PsDesiredStateConfiguration
+
+    # The Node statement specifies which targets this configuration will be applied to.
+    Node 'Mulang-Sandbox' {
+
+        # The first resource block ensures that the Web-Server (IIS) feature is enabled.
+        WindowsFeature WebServer {
+            Ensure = "Present"
+            Name   = "Web-Server"
+        }
+        
+        # The Second resource block ensures that network load balancer is enabled.
+        WindowsFeature NetworkLoadBalancer {
+            Ensure = "Present"
+            Name   = "RSAT-NLB"
+        }
+
+        # The third resource block ensures that the website content copied to the website root folder.
+        File WebsiteContent {
+            Ensure = 'Present'
+            SourcePath = 'C:\hallecraft-web\index.html'
+            DestinationPath = 'c:\inetpub\wwwroot'
+        }
+
+        # The fourth resource block ensures that dns is enabled.
+        WindowsFeature DNSServer {
+            Ensure = "Present"
+            Name   = "DNS"
+        }
+
+    }
+} 
+
+HallecraftWebTest
